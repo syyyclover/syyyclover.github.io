44b3859c9e98fb4303ba439cf98a95d1f7d7d655
syyyclover <syyy6472@gmail.com>
2025-08-13 17:03:31 +0800

    Site updated: 2025-08-13 17:03:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/09/hello-world/"];function toRandomPost(){
+var posts=["2025/08/13/工作站部门职责与应聘要求/","2025/04/11/day1/full/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
