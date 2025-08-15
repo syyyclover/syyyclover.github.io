d01d1e4205a614276cb2afc0ba589a38edb8db50
syyyclover <syyy6472@gmail.com>
2025-08-15 15:39:44 +0800

    Site updated: 2025-08-15 15:39:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/workstation/","day1/"];function toRandomPost(){
+var posts=["day1/","/workstation/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
